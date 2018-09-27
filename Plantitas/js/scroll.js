6d54eb4cf869f9d1f137ab41ccd17497e691ca47
CV01-G02-2018 <delmimorales06@gmail.com>
2018-09-27 16:17:57 -0600

    proyento terminado
    finalizacin del proyecto

--- Plantitas/js/scroll.js
+++ Plantitas/js/scroll.js
@@ -12,10 +12,13 @@ $('#inicio').on('click', function(){
     //Trabajos
     $('#trabajos2').css("background","none");
     $('#trabajos2').css("color","#878787");
+    //TBeneficio
+    $('#Beneficio2').css("background","none");
+    $('#Beneficio2').css("color","#878787");
 })
 
 $('#acercade').on('click', function(){
-    $('html, body').animate( {scrollTop: 500}, 1000);
+    $('html, body').animate( {scrollTop: 600}, 1000);
     //Inicio
     $('#inicio2').css("background","none");
     $('#inicio2').css("color","#878787");
@@ -31,7 +34,7 @@ $('#acercade').on('click', function(){
 })
 
 $('#servicios').on('click', function(){
-    $('html, body').animate( {scrollTop: 1000}, 1000);
+    $('html, body').animate( {scrollTop: 1120}, 1000);
     //Inicio
     $('#inicio2').css("background","none");
     $('#inicio2').css("color","#878787");
@@ -48,7 +51,7 @@ $('#servicios').on('click', function(){
 })
 
 $('#trabajos').on('click', function(){
-    $('html, body').animate( {scrollTop: 1500}, 1000);
+    $('html, body').animate( {scrollTop: 1700}, 1000);
     //Inicio
     $('#inicio2').css("background","none");
     $('#inicio2').css("color","#878787");
@@ -58,7 +61,29 @@ $('#trabajos').on('click', function(){
     //Servicios
     $('#servicios2').css("background","none");
     $('#servicios2').css("color","#878787");
+     //TBeneficio
+     $('#Beneficio2').css("background","none");
+     $('#Beneficio2').css("color","#878787");
     //Trabajos
     $('#trabajos2').css("background","#F45C5C");
     $('#trabajos2').css("color","white");
+    
+})
+$('#Beneficio').on('click', function(){
+    $('html, body').animate( {scrollTop: 2200}, 1000);
+    //Inicio
+    $('#inicio2').css("background","none");
+    $('#inicio2').css("color","white");
+    //Acerca de
+    $('#acercade2').css("background","none");
+    $('#acercade2').css("color","#878787");
+    //Servicios
+    $('#servicios2').css("background","none");
+    $('#servicios2').css("color","#878787");
+    //Trabajos
+    $('#trabajos2').css("background","none");
+    $('#trabajos2').css("color","#878787");
+    //TBeneficio
+    $('#Beneficio2').css("background","#F45C5C");
+    $('#Beneficio2').css("color","white");
 })
